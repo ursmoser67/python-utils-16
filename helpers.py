6a796ea978e6f33f40fb5ab9b28b253163872c99
Urs Moser <ursmoser67@gmail.com>
2026-08-14 07:29:16 +0200

    file handling utilities reorganization

--- helpers.py
+++ helpers.py
@@ -3,29 +3,31 @@ def read_file(file_path):
         return file.read()
 
 
-def write_file(file_path, data):
+def write_file(file_path, content):
     with open(file_path, 'w') as file:
-        file.write(data)
+        file.write(content)
 
 
-def append_to_file(file_path, data):
+def append_to_file(file_path, content):
     with open(file_path, 'a') as file:
-        file.write(data)
+        file.write(content)
+
+
+def clear_file(file_path):
+    with open(file_path, 'w') as file:
+        file.write('')
+
+
+def file_exists(file_path):
+    import os
+    return os.path.isfile(file_path)
+
+
+def get_file_size(file_path):
+    import os
+    return os.path.getsize(file_path) if file_exists(file_path) else 0
 
 
 def list_files(directory):
     import os
     return [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-
-
-def clear_directory(directory):
-    import os
-    for file_name in os.listdir(directory):
-        file_path = os.path.join(directory, file_name)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-
-
-def copy_file(source, destination):
-    import shutil
-    shutil.copy2(source, destination)
